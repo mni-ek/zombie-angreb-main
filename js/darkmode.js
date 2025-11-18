08363ac7650fe5496177eebc1aef20bf2e5f6670
mnikea <mni@kea.dk>
2025-11-18 10:33:09 +0100

    form done done

---
+++ js/darkmode.js
@@ -0,0 +1,21 @@
+const html = document.querySelector("html");
+const btn = document.querySelector("#theme-btn");
+
+if (localStorage.getItem("darkmode")) {
+  html.classList.toggle("dark");
+  btn.textContent = "Light";
+}
+
+function toggleTheme() {
+  html.classList.toggle("dark");
+
+  if (html.classList.contains("dark")) {
+    btn.textContent = "Light";
+    localStorage.setItem("darkmode", true);
+  } else {
+    btn.textContent = "Dark";
+    localStorage.setItem("darkmode", false);
+  }
+}
+
+btn.addEventListener("click", toggleTheme);
